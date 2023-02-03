0cf57c5f50a84f993aa3dde681439e0e07e0135a
Matias <santolarimatias@gmail.com>
2023-02-02 22:39:23 -0300

    add: style in monster, login component
    el componente login no esta terminado

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,14 +2,20 @@
 	import Counter from "./Counter.svelte";
 	import Input from "./Input.svelte";
     import Monster from "./Monster.svelte";
-	const commonProps = {maxCounter: 5} //asi no hace falta ponerlo debajo 3 veces, siendo que siempre será 5
+	import Login from "./Login.svelte";
+	let band = true;
 </script>
-<!--
-<Counter initialCounter={7} {...commonProps}/>
-<Counter initialCounter={2} {...commonProps}/>
-<Counter {...commonProps}/>
+{#if band}
+	<Login />
+{:else}
+	<div class="background">
+		<Monster />
+	</div>
+{/if}
 
-<Input/>
--->
 
-<Monster/>
+<style>
+	.background{
+        background-color: rgb(27, 79, 85);
+    }
+</style>
--- frontend/src/Monster.svelte
+++ frontend/src/Monster.svelte
@@ -1,16 +1,16 @@
 <script>
-    export let especie = '';
-    export let monstruos = [];
+    let especie = 'Todos';
+    let monstruos = [];
+    let especies = ['Todos','Primigenios','Dioses Exteriores', 'Dioses Arquetípicos']
 
-    const handleInput = (event) => especie = event.target.value
+    //const handleInputEspecie = (event) => especie = event.target.value
+    //const handleInputNombreMonstruo = (event) => nombreMonstruo = event.target.value
 
-    function handleClick(){
-        fetch(`http://localhost:3000/api/monstruos`)
+    function getOneMonstruo(){
+        fetch(`http://localhost:3000/api/monstruos/monstruoNombre/${nombreMonstruo}`)
         .then(res => res.json())
         .then(apiResponse => {
-        monstruos = apiResponse.body || []
-        //console.log(`Monstruos: ${JSON.stringify(monstruos)}`)
-        console.log(especie)
+            monstruos = apiResponse.body || []
         })
     }
 
@@ -18,43 +18,81 @@
         fetch(`http://localhost:3000/api/monstruos`)
         .then(res => res.json())
         .then(apiResponse => {
-        monstruos = apiResponse.body || []
+            monstruos = apiResponse.body || []
         })
     }
     function getMonstruosByEspecie(){
-        fetch(`http://localhost:3000/api/monstruos/monstruoByEspecie/637bd550cdf8ea0527a5548f`)
+        fetch(`http://localhost:3000/api/monstruos/monstruoByEspecie/${especie}`)
         .then(res => res.json())
         .then(apiResponse => {
-        monstruos = apiResponse.body || []
+            monstruos = apiResponse.body || []
         })
     }
 
     //Renderizado condicional para filtrar monstruos por especie
-    $:if(especie === 'Primigenios'){
+    $:if(especie != 'Todos'){
         getMonstruosByEspecie()
     }
-    else{fetch(`http://localhost:3000/api/monstruos`)
+    else{
         getAllMonstruos()
     }
     //fin renderizado condicional para filtrar monstruos por especie
-
 </script>
 
-<input placeholder="Buscar por especie" value={especie} on:input={handleInput}>
-<button on:click={handleClick}>Mostrar Monstruos</button>
-<button on:click={getMonstruosByEspecie}>Mostrar Monstruos Primigenios</button>
-
-
-{#each monstruos as monster}
-    <p>{monster.nombre}</p>
-    <p>{monster.estado}</p>
-    <br>
-{/each}
-
 <!--
-<p>
-    {#each monstruos as monstruo}
-        {monstruo.nombre}
-    {/each}
-</p>
+<input placeholder="Buscar monstruo" value={nombreMonstruo} on:input={handleInputNombreMonstruo}>
 -->
+
+<div class="container">
+    <div class="select-especie">
+        <select bind:value={especie}>
+            {#each especies as value}<option {value}>{value}</option>{/each}
+        </select>
+    </div>
+    <br>
+
+    <div class="row justify-content-center align-items-center g-2">
+        <div class="col"> 
+            <h1>
+                Monstruos
+            </h1>
+        </div>
+    </div>
+
+    {#each monstruos as monster}
+        <div class="column col-sm-12 col-md-6 col-lg-4 listado_horizontal">
+            <div class="card text-bg-dark" style="width: 300px">
+            <img class="card-img-top imagenes_monstruos img-thumbnail" src={monster.urlImagen} alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title"> {monster.nombre} </h4>
+                <p class="card-text">Detalle: {monster.detalle}</p>
+                <p class="card-text">Nivel de Amenaza: {monster.nivelAmenaza}</p>
+                <p class="card-text">Estado: {monster.estado}</p>
+                <p class="card-text">Nombres Alternativos: {monster.nombresAlternativos}</p>
+                <a href="#" class="btn btn-success">See Profile</a>
+            </div>
+            <br>
+            </div>
+        </div>
+    {/each}
+</div>
+<style>
+    .listado_horizontal{
+        display: inline-block;
+        margin-top: 20px;
+    }
+    .imagenes_monstruos{
+        height: 399px;
+        width: 399px;
+    }
+    h1{
+        margin-top: 10px;
+    }
+    
+    .select-especie{
+        display: flex;
+        justify-content: flex-end;
+        padding-top: 30px;
+        padding-right: 80px;
+    }
+</style>
